2f88d06bf113cc0e89a38f7ff7fc7c253d0d40e0
devheyrin <beforerin@gmail.com>
2021-07-30 15:21:29 +0900

    [전혜린] 게시판 뷰 전환
    - templates 폴더 아래에 게시판 관련 html추가
    - 글쓰기, 글 수정, 글 삭제 기능 구현 완료
    - 댓글 쓰기, 댓글 삭제 기능 구현 완료

--- src/main/java/com/garb/gbcollector/web/controller/BoardController.java
+++ src/main/java/com/garb/gbcollector/web/controller/BoardController.java
@@ -1,18 +1,10 @@
 package com.garb.gbcollector.web.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -27,6 +19,7 @@ import com.garb.gbcollector.web.service.BoardService;
 import com.garb.gbcollector.web.vo.BoardReplyVO;
 import com.garb.gbcollector.web.vo.BoardVO;
 
+@RequestMapping("board")
 @Controller
 
 public class BoardController {
@@ -42,251 +35,125 @@ public class BoardController {
 	@Autowired
 	BoardService boardService;
 	
-
-	/* 모든 글 보기 */
-	@RequestMapping(value = "boardList.do", 
-			method = { RequestMethod.POST }, 
-			produces = "application/text; charset=utf8")
-	@ResponseBody
-	public String boardList(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("요청 들어옴");
-		//글 리스트를 담기 위한 json객체, json배열, listmap선언 
-		JSONObject jsonObject = new JSONObject();
-		//자유게시판용
-		JSONArray jsonArrayB = new JSONArray();
-		List<Map<String, Object>> listMapB = new ArrayList<Map<String, Object>>();
-		//질문게시판용
-		JSONArray jsonArrayQ = new JSONArray();
-		List<Map<String, Object>> listMapQ = new ArrayList<Map<String, Object>>();
-		//날짜 형식 지정
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH시 mm분");
-
-		String[] setB = {"title", "content", "nickname", "postdate", "postno", "parentno", "reply_cnt"};
-		String[] setQ = {"title", "content", "nickname", "postdate", "postno", "parentno", "reply_cnt"};
-		
-		//자유게시판 글 리스트 받아오기 
-		List<BoardVO> postListB = boardService.listPostB();
-		//질문게시판 글 리스트 받아오기
-		List<BoardVO> postListQ = boardService.listPostQ();
-		//System.out.println("postList:"+postList);
-		
-		//자유게시판 글 처리 
-		for (BoardVO post : postListB) {
-			//리스트 안에서 for문을 돌면서 map객체에 제목, 내용, 닉네임, 날짜 추가 
-			Map<String, Object> mapB = new HashMap<String, Object>();
-			try {
-				mapB.put(setB[0], post.getTitle());
-				mapB.put(setB[1], post.getContent());
-				mapB.put(setB[2], post.getNickname());
-				//map.put(set[3], post.getPostdate().toString());
-				mapB.put(setB[3], dateFormat.format(post.getPostdate()));
-				//날짜 데이터는 getPostdate로 받아올 경우 String이 아니기 때문에 바로 보낼 수 없음. toString을 통해 String으로 바꿔주어야 함. 
-				mapB.put(setB[4], post.getPostno());
-				mapB.put(setB[5], post.getParentno());
-				mapB.put(setB[6], post.getReply_cnt());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//미리 만들어둔 listmap객체에 담기 
-			listMapB.add(mapB);
-			//System.out.println("listmap:"+listMap);
-		}
-		for(Map<String, Object> jsonItem : listMapB) {
-			//for 문을 돌면서 jsonArray에 listmap을 담기
-			jsonArrayB.add(jsonItem);
-		}
-		
-		//질문게시판 글 처리 
-		for (BoardVO post : postListQ) {
-			//리스트 안에서 for문을 돌면서 map객체에 제목, 내용, 닉네임, 날짜 추가 
-			Map<String, Object> mapQ = new HashMap<String, Object>();
-			try {
-				mapQ.put(setQ[0], post.getTitle());
-				mapQ.put(setQ[1], post.getContent());
-				mapQ.put(setQ[2], post.getNickname());
-				//map.put(set[3], post.getPostdate().toString());
-				mapQ.put(setQ[3], dateFormat.format(post.getPostdate()));
-				//날짜 데이터는 getPostdate로 받아올 경우 String이 아니기 때문에 바로 보낼 수 없음. toString을 통해 String으로 바꿔주어야 함. 
-				mapQ.put(setQ[4], post.getPostno());
-				mapQ.put(setQ[5], post.getParentno());
-				mapQ.put(setQ[6], post.getReply_cnt());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//미리 만들어둔 listmap객체에 담기 
-			listMapQ.add(mapQ);
-			//System.out.println("listmap:"+listMap);
-		}
-		for(Map<String, Object> jsonItem : listMapQ) {
-			//for 문을 돌면서 jsonArray에 listmap을 담기
-			jsonArrayQ.add(jsonItem);
-		}
-		//System.out.println("jsonArray:"+jsonArray);
-		//jsonArray를 jsonObject객체에 담아 보내기 
-		jsonObject.put("postListB", jsonArrayB);
-		jsonObject.put("postListQ", jsonArrayQ);
-		System.out.println("jsonObject:"+jsonObject);
-		return jsonObject.toJSONString();
+	/* 전체게시판 조회 */
+	@RequestMapping(value="/boardlist", method=RequestMethod.GET)
+	public ModelAndView boardList(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, HttpSession sessoion) {
+		System.out.println("board/list진입");
+		List<BoardVO> b_boards;
+		b_boards = boardService.listPostB();
+		List<BoardVO> q_boards;
+		q_boards = boardService.listPostQ();
+		mav.addObject("b_boards", b_boards);
+		mav.addObject("q_boards", q_boards);
+		mav.setViewName("board/list");
+		return mav;
 	}
 	
-	
-	/* 자유게시판 글 전체 보기 */
-	@RequestMapping(value = "boardListB.do", 
-			method = { RequestMethod.POST }, 
-			produces = "application/text; charset=utf8")
-	@ResponseBody
-	public String boardListB(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("요청 들어옴!");
-		//글 리스트를 담기 위한 json객체, json배열, listmap선언 
-		JSONObject jsonObject = new JSONObject();
-		//자유게시판용
-		JSONArray jsonArrayB = new JSONArray();
-		List<Map<String, Object>> listMapB = new ArrayList<Map<String, Object>>();
-		//날짜 형식 지정
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH시 mm분");
-		String[] setB = {"title", "content", "nickname", "postdate", "postno", "parentno", "reply_cnt"};
-		//자유게시판 글 리스트 받아오기 
-		List<BoardVO> postListB = boardService.listPostBAll();
-		
-		//자유게시판 글 처리 
-		for (BoardVO post : postListB) {
-			//리스트 안에서 for문을 돌면서 map객체에 제목, 내용, 닉네임, 날짜 추가 
-			Map<String, Object> mapB = new HashMap<String, Object>();
-			try {
-				mapB.put(setB[0], post.getTitle());
-				mapB.put(setB[1], post.getContent());
-				mapB.put(setB[2], post.getNickname());
-				//map.put(set[3], post.getPostdate().toString());
-				mapB.put(setB[3], dateFormat.format(post.getPostdate()));
-				//날짜 데이터는 getPostdate로 받아올 경우 String이 아니기 때문에 바로 보낼 수 없음. toString을 통해 String으로 바꿔주어야 함. 
-				mapB.put(setB[4], post.getPostno());
-				mapB.put(setB[5], post.getParentno());
-				mapB.put(setB[6], post.getReply_cnt());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//미리 만들어둔 listmap객체에 담기 
-			listMapB.add(mapB);
-			//System.out.println("listmap:"+listMap);
-		}
-		for(Map<String, Object> jsonItem : listMapB) {
-			//for 문을 돌면서 jsonArray에 listmap을 담기
-			jsonArrayB.add(jsonItem);
-		}
-		//System.out.println("jsonArray:"+jsonArray);
-		//jsonArray를 jsonObject객체에 담아 보내기 
-		jsonObject.put("postListB", jsonArrayB);
-		System.out.println("jsonObject:"+jsonObject);
-		return jsonObject.toJSONString();
+	/* 자유게시판 조회 */
+	@RequestMapping(value="/bulletin_boardlist", method=RequestMethod.GET)
+	public ModelAndView boardListB(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, HttpSession sessoion) {
+		System.out.println("board/bulletin_boardlist진입");
+		List<BoardVO> b_boards;
+		b_boards = boardService.listPostBAll();
+		mav.addObject("b_boards", b_boards);
+		mav.setViewName("board/bulletin_boardlist");
+		return mav;
 	}
 	
-	/* 질문게시판 글 전체 보기 */
-	@RequestMapping(value = "boardListQ.do", 
-			method = { RequestMethod.POST }, 
-			produces = "application/text; charset=utf8")
-	@ResponseBody
-	public String boardListQ(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("요청 들어옴");
-		//글 리스트를 담기 위한 json객체, json배열, listmap선언 
-		JSONObject jsonObject = new JSONObject();
-		//자유게시판용
-		JSONArray jsonArrayQ = new JSONArray();
-		List<Map<String, Object>> listMapQ = new ArrayList<Map<String, Object>>();
-		//날짜 형식 지정
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH시 mm분");
-		String[] setQ = {"title", "content", "nickname", "postdate", "postno", "parentno", "reply_cnt"};
-		//자유게시판 글 리스트 받아오기 
-		List<BoardVO> postListQ = boardService.listPostQAll();
-		
-		//자유게시판 글 처리 
-		for (BoardVO post : postListQ) {
-			//리스트 안에서 for문을 돌면서 map객체에 제목, 내용, 닉네임, 날짜 추가 
-			Map<String, Object> mapQ = new HashMap<String, Object>();
-			try {
-				mapQ.put(setQ[0], post.getTitle());
-				mapQ.put(setQ[1], post.getContent());
-				mapQ.put(setQ[2], post.getNickname());
-				//map.put(set[3], post.getPostdate().toString());
-				mapQ.put(setQ[3], dateFormat.format(post.getPostdate()));
-				//날짜 데이터는 getPostdate로 받아올 경우 String이 아니기 때문에 바로 보낼 수 없음. toString을 통해 String으로 바꿔주어야 함. 
-				mapQ.put(setQ[4], post.getPostno());
-				mapQ.put(setQ[5], post.getParentno());
-				mapQ.put(setQ[6], post.getReply_cnt());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//미리 만들어둔 listmap객체에 담기 
-			listMapQ.add(mapQ);
-			//System.out.println("listmap:"+listMap);
-		}
-		for(Map<String, Object> jsonItem : listMapQ) {
-			//for 문을 돌면서 jsonArray에 listmap을 담기
-			jsonArrayQ.add(jsonItem);
-		}
-		//System.out.println("jsonArray:"+jsonArray);
-		//jsonArray를 jsonObject객체에 담아 보내기 
-		jsonObject.put("postListQ", jsonArrayQ);
-		System.out.println("jsonObject:"+jsonObject);
-		return jsonObject.toJSONString();
+	/* 질문게시판 조회 */
+	@RequestMapping(value="/question_boardlist", method=RequestMethod.GET)
+	public ModelAndView boardListQ(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, HttpSession sessoion) {
+		System.out.println("board/question_boardlist진입");
+		List<BoardVO> q_boards;
+		q_boards = boardService.listPostQAll();
+		mav.addObject("q_boards", q_boards);
+		mav.setViewName("board/question_boardlist");
+		return mav;
 	}
+	
+	/* 글쓰기 화면 요청 */
+	@RequestMapping(value="/write", method = RequestMethod.GET)
+	public ModelAndView viewWritePage(ModelAndView mav, HttpSession session) {
+		/* 세션 체크해 로그인된 사용자가 아니면 경고창/에러페이지 반환 */
+		
+		mav.setViewName("board/write");
+		return mav;
+	}
+	
 
 	/*글쓰기*/
-	@RequestMapping(value = "registerPost.do", 
-			method= {RequestMethod.POST},
+	@RequestMapping(value = "/write", 
+			method= RequestMethod.POST,
 			produces = "application/text; charset=utf8")	
 	
 	@ResponseBody
-	public String registerPost(HttpServletRequest request,
-			HttpServletResponse response) {
-
-		//request로부터 게시판이름, 제목, 내용 얻기 
-		boardname = request.getParameter("boardname");
-		title = request.getParameter("title");
-		//nickname = request.getParameter("nickname");
-		content = request.getParameter("content");
+	public ModelAndView writePost(ModelAndView mav, @ModelAttribute("BoardVO") BoardVO boardVO, 
+			HttpServletRequest request,
+			HttpSession session) {
 		
-		//session을 얻어 session으로부터 닉네임 얻기 
-		HttpSession session = request.getSession();
-		nickname = (String) session.getAttribute("memnickname");
-		//System.out.println(nickname);		
-		try {
-			BoardVO post = new BoardVO(boardname, title, nickname, content);
-			boardService.insertPost(post);
-			return "글이 성공적으로 등록되었습니다";
-		}catch (Exception e) {
-			//글 등록 실패 메시지 띄우기 
-			return e.getMessage();
+		System.out.println("writePost 진입");
+		
+		String boardname = request.getParameter("boardname");
+		boardVO.setBoardname(boardname);
+		System.out.println(boardname);
+		System.out.println(boardVO.getBoardname());
+		
+		String title = request.getParameter("title");
+		boardVO.setTitle(title);
+		System.out.println(title);
+		System.out.println(boardVO.getTitle());
+
+		String nickname = request.getParameter("nickname");
+		boardVO.setNickname(nickname);
+		
+		String content = request.getParameter("content");
+		boardVO.setContent(content);
+		System.out.println(boardVO);
+		
+//		//session을 얻어 session으로부터 닉네임 얻기 
+//		HttpSession session = request.getSession();
+//		nickname = (String) session.getAttribute("memnickname");
+//		//System.out.println(nickname);		
+
+		boardService.insertPost(boardVO);
+		
+		String bulletin_board = "자유게시판";
+		String question_board = "질문게시판";
+		
+		//자유게시판에 쓴 글이면 자유게시판 리스트로 이동, 질문게시판에 쓴 글이면 질문게시판 리스트로 이동
+		if(bulletin_board.equals(boardVO.getBoardname())) {
+			mav.setViewName("redirect:/board/bulletin_boardlist");
+		}else if(question_board.equals(boardVO.getBoardname())) {
+			mav.setViewName("redirect:/board/question_boardlist");
 		}
+		return mav;
 	}
 	
+
 	/*글+댓글 보기
 	 *선택한 글과 그 글에 달린 댓글을 모두 가져와 한 페이지에 출력*/
-	@RequestMapping(value = "viewPost", method= {RequestMethod.GET})
+	@RequestMapping(value = "viewpost", method= {RequestMethod.GET})
 	@ResponseBody
 	public ModelAndView viewPostPage(@RequestParam("postno") int postno, HttpServletRequest request) {
 		//postno를 파라미터로 받아와서 해당 글 조회 
-		System.out.println(postno+"번 글 보기 요청 들어옴");
+		System.out.println(postno+"번 글 보기");
 		ModelAndView mav = new ModelAndView();
 		//선택한 postno와 일치하는 게시글 조회 
 		BoardVO boardVO = boardService.viewPost(postno);
 		//게시글 객체를 post라는 이름으로 뷰페이지에 추가 
 		mav.addObject("post", boardVO);
-		//뷰 이름 지정- viewPost.jsp
-		mav.setViewName("viewPost");
-		System.out.println("boardVO"+boardVO);
+		mav.setViewName("board/view");
+		//System.out.println(boardVO);
 		
 		//현재 게시글에 대한 댓글을 모두 가져오기. 한 게시글당 댓글이 하나 이상이기 때문에 타입은 List이다.  
 		List<BoardReplyVO> boardReplyVO = boardService.viewReply(postno);
-		System.out.println("댓글VO"+boardReplyVO);
-		//댓글 리스트를 reply라는 이름으로 뷰페이지에 추가 
-		mav.addObject("reply", boardReplyVO);
-		System.out.println("댓글VO"+boardReplyVO);
+//		//댓글 리스트를 reply라는 이름으로 뷰페이지에 추가 
+		mav.addObject("commentList", boardReplyVO);
+		//System.out.println(boardReplyVO);
 		//세션으로부터 닉네임 얻기
 		HttpSession session = request.getSession();
 		nickname = (String) session.getAttribute("memnickname");
-		System.out.println("nickname"+nickname);
-		
-		//뷰페이지를 반환(viewPost.jsp호출됨)
+		//System.out.println(nickname);
 		return mav;
 	}
 	
@@ -296,30 +163,32 @@ public class BoardController {
 		System.out.println(postno+"번 글 수정");
 		BoardVO boardVO = boardService.viewPost(postno);
 		modelMap.addAttribute("post", boardVO);
-		return "updatePost";
+		return "board/modify";
 	}
 	
 	/*글 수정 후 저장*/
 	@RequestMapping(value = "updatePostSave")
 	public String updatePostSave(@ModelAttribute("post") BoardVO boardVO) {
+		System.out.println(boardVO.getPostno()+"번 글 수정 진입");
 		boardService.updatePost(boardVO);
-		return "redirect:/viewPost?postno="+boardVO.getPostno();
+		return "redirect:/board/viewpost?postno="+boardVO.getPostno();
 	}
 	
 	/*글 삭제*/
 	@RequestMapping(value="deletePost", 
 					method= {RequestMethod.POST})
-	public String deletePost(@RequestParam("postno") Integer postno, HttpServletRequest request) {
+	public ModelAndView deletePost(@RequestParam("postno") Integer postno, HttpServletRequest request, ModelAndView mav) {
 		
 		
-		HttpSession session = request.getSession();
-		nickname = (String) session.getAttribute("memnickname");
+//		HttpSession session = request.getSession();
+//		nickname = (String) session.getAttribute("memnickname");
 		//System.out.println(nickname);
 		
 		System.out.println(postno+"번 글 삭제");
 		boardService.deletePost(postno);
 		//글 삭제 후 전체 글 리스트 페이지로 이동
-		return "redirect:/html/boardPage.html";
+		mav.setViewName("redirect:/board/bulletin_boardlist");
+		return mav;
 	}
 	
 	/*댓글 등록+수정*/
@@ -335,7 +204,7 @@ public class BoardController {
 		
 		boardService.insertBoardReply(boardReplyVO);
 		System.out.println("요청 들어옴");
-		return "redirect:/viewPost?postno="+boardReplyVO.getPostno();
+		return "redirect:/board/viewpost?postno="+boardReplyVO.getPostno();
 	}
 	
 	/*댓글 삭제*/
@@ -345,9 +214,7 @@ public class BoardController {
 		System.out.println(reno+"번 댓글 삭제");
 		boardService.deleteReply(reno);
 		//댓글 삭제 후 페이지 새로고침 
-		return "redirect:/viewPost?postno="+boardReplyVO.getPostno();
+		return "redirect:/board/viewpost?postno="+boardReplyVO.getPostno();
 	}
-	
-	
 	
 }
