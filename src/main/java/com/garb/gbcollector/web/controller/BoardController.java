1f439266e96e2f4116588d69a76fb1fdc6f0eca0
devheyrin <beforerin@gmail.com>
2021-08-27 00:53:21 +0900

    [전혜린] 게시판 파일 업로드 기능 수정
    - 이미지파일만 업로드 가능하도록 수정

--- src/main/java/com/garb/gbcollector/web/controller/BoardController.java
+++ src/main/java/com/garb/gbcollector/web/controller/BoardController.java
@@ -35,12 +35,12 @@ import com.garb.gbcollector.web.vo.BoardVO;
 @Controller
 
 public class BoardController {
-	
+
 	@Autowired
 	BoardService boardService;
-	
+
 	/* 전체게시판 조회 */
-	@GetMapping(value="/boardlist")
+	@GetMapping(value = "/boardlist")
 	public ModelAndView boardList(ModelAndView mav, HttpSession session) {
 		System.out.println("board/list진입");
 		List<BoardVO> b_boards;
@@ -50,12 +50,12 @@ public class BoardController {
 		mav.addObject("b_boards", b_boards);
 		mav.addObject("q_boards", q_boards);
 		mav.setViewName("board/list");
-	
+
 		return mav;
 	}
-	
+
 	/* 자유게시판 조회 */
-	@GetMapping(value="/bulletin_boardlist")
+	@GetMapping(value = "/bulletin_boardlist")
 	public ModelAndView boardListB(ModelAndView mav, HttpSession session) {
 		System.out.println("board/bulletin_boardlist진입");
 		List<BoardVO> b_boards;
@@ -64,9 +64,9 @@ public class BoardController {
 		mav.setViewName("board/bulletin_boardlist");
 		return mav;
 	}
-	
+
 	/* 질문게시판 조회 */
-	@GetMapping(value="/question_boardlist")
+	@GetMapping(value = "/question_boardlist")
 	public ModelAndView boardListQ(ModelAndView mav, HttpSession session) {
 		System.out.println("board/question_boardlist진입");
 		List<BoardVO> q_boards;
@@ -75,91 +75,91 @@ public class BoardController {
 		mav.setViewName("board/question_boardlist");
 		return mav;
 	}
-	
+
 	/* 글쓰기 화면 요청 */
-	@GetMapping(value="/write")
+	@GetMapping(value = "/write")
 	public ModelAndView viewWritePage(ModelAndView mav, HttpSession session) {
-		
+
 		/* 토큰값을 생성해 session과 view에 저장 */
 		addTokenToSession(mav, session);
-		
+
 		mav.setViewName("board/write");
 		return mav;
 	}
-	
-	
+
 	public void addTokenToSession(ModelAndView mav, HttpSession session) {
 		session.setAttribute("CSRF_TOKEN", UUID.randomUUID().toString());
 		String token = (String) session.getAttribute("CSRF_TOKEN");
 		System.out.println("CSRF_TOKEN: " + token);
 		mav.addObject("token", token);
 	}
-	
-	
-	/*글쓰기*/
+
+	/* 글쓰기 */
 	@PostMapping(value = "/write")
-	public ModelAndView writePost(ModelAndView mav, @ModelAttribute("BoardVO") BoardVO boardVO, 
-			HttpSession session, MultipartHttpServletRequest req) 
-	{
+	public ModelAndView writePost(ModelAndView mav, @ModelAttribute("BoardVO") BoardVO boardVO, HttpSession session,
+			MultipartHttpServletRequest req) {
 		System.out.println("writePost 진입");
-		
+
 		String uploadPath = System.getProperty("user.home") + "/files/";
 		System.out.println(uploadPath);
 		MultipartFile file = req.getFile("file");
-		if(file != null && !"".equals(file.getOriginalFilename())) {
-			String fileName = file.getOriginalFilename();
-			System.out.println("파일 이름: " + fileName);
-			File uploadFile = new File(uploadPath + fileName);
-			
-			//폴더 경로
-			File Folder = new File(uploadPath);
-			
-			if(!Folder.exists()) {
-				try {
-					Folder.mkdir(); // 폴더 생성
-					System.out.println("폴더 생성");
-				} catch (Exception e) {
-					e.printStackTrace();
+		if (file != null && !"".equals(file.getOriginalFilename())) {
+			String fileName = file.getOriginalFilename().toLowerCase();
+
+			if (fileName != null) {
+				if (fileName.endsWith(".jpg") || fileName.endsWith(".png") || fileName.endsWith(".gif")
+						|| fileName.endsWith("jpeg")) {
+
+					System.out.println("파일 이름: " + fileName);
+					File uploadFile = new File(uploadPath + fileName);
+
+					// 폴더 경로
+					File Folder = new File(uploadPath);
+
+					if (!Folder.exists()) {
+						try {
+							Folder.mkdir(); // 폴더 생성
+							System.out.println("폴더 생성");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					if (uploadFile.exists()) {
+						fileName = new Date().getTime() + fileName;
+						uploadFile = new File(uploadPath + fileName);
+						System.out.println("파일 업로드: " + uploadPath + fileName);
+					}
+					try {
+						file.transferTo(uploadFile);
+					} catch (Exception e) {
+						System.out.println("업로드 에러");
+						System.out.println(e);
+					}
+					boardVO.setFilename(fileName);
 				}
 			}
-			if (uploadFile.exists()) {
-				fileName = new Date().getTime() + fileName;
-				uploadFile = new File(uploadPath + fileName);
-				System.out.println("파일 업로드: " + uploadPath + fileName);
-			}
-			try {
-				file.transferTo(uploadFile);
-			} catch (Exception e) {
-				System.out.println("업로드 에러");
-				System.out.println(e);
-			}
-			boardVO.setFilename(fileName);			
-		}else if(file == null) {
-			boardVO.setFilename(" ");
 		}
-		
-		
+
 		String nickname = (String) session.getAttribute("memnickname");
-		boardVO.setNickname(nickname);	
-		
-		if(boardVO.getIsanon()==null) {
+		boardVO.setNickname(nickname);
+
+		if (boardVO.getIsanon() == null) {
 			boardVO.setIsanon("0");
 		}
-				
+
 		/* form으로 전송된 요청으로부터 token값을 얻어오기 */
 		String token = req.getParameter("_csrf");
 		System.out.println("_csrf: " + token);
-		
-		
+
 		if (session.getAttribute("CSRF_TOKEN").equals(token)) {
 			System.out.println("토큰 일치");
 			boardService.insertPost(boardVO);
 			String bulletin_board = "자유게시판";
 			String question_board = "질문게시판";
-			//자유게시판에 쓴 글이면 자유게시판 리스트로 이동, 질문게시판에 쓴 글이면 질문게시판 리스트로 이동
-			if(bulletin_board.equals(boardVO.getBoardname())) {
+			// 자유게시판에 쓴 글이면 자유게시판 리스트로 이동, 질문게시판에 쓴 글이면 질문게시판 리스트로 이동
+			if (bulletin_board.equals(boardVO.getBoardname())) {
 				mav.setViewName("redirect:/board/bulletin_boardlist");
-			}else if(question_board.equals(boardVO.getBoardname())) {
+			} else if (question_board.equals(boardVO.getBoardname())) {
 				mav.setViewName("redirect:/board/question_boardlist");
 			}
 			return mav;
@@ -168,9 +168,9 @@ public class BoardController {
 		String msg = "비정상적인 접근입니다.";
 		mav.addObject("msg", msg);
 		mav.setViewName("board/error");
-		return mav;	
+		return mav;
 	}
-	
+
 //	/*글쓰기*/
 //	@PostMapping(value = "/write")
 //	public ModelAndView writePost(ModelAndView mav, @ModelAttribute("BoardVO") BoardVO boardVO, 
@@ -209,48 +209,44 @@ public class BoardController {
 //		mav.setViewName("board/error");
 //		return mav;	
 //	}
-	
-	/*글+댓글 보기*/
+
+	/* 글+댓글 보기 */
 	@GetMapping(value = "viewpost")
-	public ModelAndView viewPostPage(@RequestParam("postno") int postno, HttpServletResponse res, HttpSession session) 
-	{
-		System.out.println(postno+"번 글 보기");
+	public ModelAndView viewPostPage(@RequestParam("postno") int postno, HttpServletResponse res, HttpSession session) {
+		System.out.println(postno + "번 글 보기");
 		ModelAndView mav = new ModelAndView();
 		BoardVO boardVO = boardService.viewPost(postno);
 		System.out.println(boardVO);
 		System.out.println(boardVO.getFilename());
 		mav.addObject("post", boardVO);
-		
+
 		addTokenToSession(mav, session);
-		
-		//현재 게시글에 대한 댓글을 모두 가져오기. 한 게시글당 댓글이 하나 이상이기 때문에 타입은 List이다.  
+
+		// 현재 게시글에 대한 댓글을 모두 가져오기. 한 게시글당 댓글이 하나 이상이기 때문에 타입은 List이다.
 		List<BoardReplyVO> boardReplyVO = boardService.viewReply(postno);
-		//댓글 리스트를 reply라는 이름으로 뷰페이지에 추가 
+		// 댓글 리스트를 reply라는 이름으로 뷰페이지에 추가
 		mav.addObject("commentList", boardReplyVO);
-		
-		
-		
-		//세션으로부터 닉네임 얻기
+
+		// 세션으로부터 닉네임 얻기
 		String nickname = (String) session.getAttribute("memnickname");
 		mav.addObject("login_nickname", nickname);
 		mav.setViewName("board/view");
 		return mav;
 	}
-	
-	
+
 	/* 이미지 출력 */
 	@GetMapping(value = "display")
-	public ResponseEntity<Resource> display(@RequestParam("filename") String filename){
+	public ResponseEntity<Resource> display(@RequestParam("filename") String filename) {
 		String path = "C:\\Users\\user\\files\\";
 		Resource resource = new FileSystemResource(path + filename);
-		
+
 		System.out.println(resource);
-		if(!resource.exists())
+		if (!resource.exists())
 			return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
-		
+
 		HttpHeaders header = new HttpHeaders();
 		Path filePath = null;
-		
+
 		try {
 			filePath = Paths.get(path + filename);
 			header.add("Content-Type", Files.probeContentType(filePath));
@@ -259,32 +255,29 @@ public class BoardController {
 		}
 		return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
 	}
-	
-	
-	
-	
-	/*글 수정 페이지 가져오기 */
+
+	/* 글 수정 페이지 가져오기 */
 	@GetMapping(value = "updatePost")
 	public ModelAndView updatePost(@RequestParam("postno") int postno, ModelAndView mav, HttpSession session) {
-		System.out.println(postno+"번 글 수정");
-		
+		System.out.println(postno + "번 글 수정");
+
 		addTokenToSession(mav, session);
-		
+
 		BoardVO boardVO = boardService.viewPost(postno);
 		mav.addObject("post", boardVO);
 		mav.setViewName("board/modify");
 		return mav;
 	}
-	
-	/*글 수정 후 저장*/
+
+	/* 글 수정 후 저장 */
 	@PostMapping(value = "updatePostSave")
-	public String updatePostSave(@ModelAttribute("post") BoardVO boardVO, 
-			HttpSession session, MultipartHttpServletRequest req) {
-		System.out.println(boardVO.getPostno()+"번 글 수정 진입");
-		
+	public String updatePostSave(@ModelAttribute("post") BoardVO boardVO, HttpSession session,
+			MultipartHttpServletRequest req) {
+		System.out.println(boardVO.getPostno() + "번 글 수정 진입");
+
 		String uploadPath = System.getProperty("user.home") + "/files/";
 		String orgFileName = req.getParameter("orgFile");
-		
+
 		MultipartFile newFile = req.getFile("file");
 		if (newFile != null) {
 			String newFileName = newFile.getOriginalFilename();
@@ -309,69 +302,64 @@ public class BoardController {
 				boardVO.setFilename(newFileName);
 			}
 		}
-		
-		
-		
+
 		String nickname = (String) session.getAttribute("memnickname");
-		boardVO.setNickname(nickname);	
-		if(boardVO.getIsanon()==null) {
+		boardVO.setNickname(nickname);
+		if (boardVO.getIsanon() == null) {
 			boardVO.setIsanon("0");
 		}
-		
+
 		String token = req.getParameter("_csrf");
 		System.out.println("_csrf: " + token);
-		
+
 		if (req.getSession().getAttribute("CSRF_TOKEN").equals(token)) {
 			System.out.println("토큰 일치");
 			boardService.updatePost(boardVO);
-			return "redirect:/board/viewpost?postno="+boardVO.getPostno();
+			return "redirect:/board/viewpost?postno=" + boardVO.getPostno();
 		}
-		//에러페이지 반환
+		// 에러페이지 반환
 		return null;
-		
-		
-		
+
 	}
-	
-	/*글 삭제*/
-	@PostMapping(value="deletePost")
-	public ModelAndView deletePost(@RequestParam("postno") Integer postno, HttpServletRequest request, ModelAndView mav) 
-	{
-		System.out.println(postno+"번 글 삭제");
+
+	/* 글 삭제 */
+	@PostMapping(value = "deletePost")
+	public ModelAndView deletePost(@RequestParam("postno") Integer postno, HttpServletRequest request,
+			ModelAndView mav) {
+		System.out.println(postno + "번 글 삭제");
 		boardService.deletePost(postno);
-		
+
 		String boardname = request.getParameter("boardname");
 		String bulletin_board = "자유게시판";
 		String question_board = "질문게시판";
-		//자유게시판에 쓴 글이면 자유게시판 리스트로 이동, 질문게시판에 쓴 글이면 질문게시판 리스트로 이동
-		if(bulletin_board.equals(boardname)) {
+		// 자유게시판에 쓴 글이면 자유게시판 리스트로 이동, 질문게시판에 쓴 글이면 질문게시판 리스트로 이동
+		if (bulletin_board.equals(boardname)) {
 			mav.setViewName("redirect:/board/bulletin_boardlist");
-		}else if(question_board.equals(boardname)) {
+		} else if (question_board.equals(boardname)) {
 			mav.setViewName("redirect:/board/question_boardlist");
 		}
 		return mav;
 	}
-	
-	/*댓글 등록+수정*/
+
+	/* 댓글 등록+수정 */
 	/* BoardService에서 getReno()가 null이면 댓글 insert, 번호가 있으면 update 수행 */
 	@PostMapping(value = "boardReplySave")
 	public String boardReplySave(BoardReplyVO boardReplyVO, HttpSession session) {
-		
+
 		String nickname = (String) session.getAttribute("memnickname");
 		boardReplyVO.setNickname(nickname);
-		
+
 		boardService.insertBoardReply(boardReplyVO);
 		System.out.println("요청 들어옴");
-		return "redirect:/board/viewpost?postno="+boardReplyVO.getPostno();
+		return "redirect:/board/viewpost?postno=" + boardReplyVO.getPostno();
 	}
-	
-	/*댓글 삭제*/
-	@PostMapping(value="deleteReply")
-	public String deleteReply(@RequestParam("reno") Integer reno, @RequestParam("postno") int postno) 
-	{		
-		System.out.println(reno+"번 댓글 삭제");
+
+	/* 댓글 삭제 */
+	@PostMapping(value = "deleteReply")
+	public String deleteReply(@RequestParam("reno") Integer reno, @RequestParam("postno") int postno) {
+		System.out.println(reno + "번 댓글 삭제");
 		boardService.deleteReply(reno);
-		return "redirect:/board/viewpost?postno="+postno;
+		return "redirect:/board/viewpost?postno=" + postno;
 	}
-	
+
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,61 +1,54 @@
-$(document).ready(function(){
-	$("#cancleBtn").click(function(){
-		if(confirm("글 작성을 취소하시겠습니까? 작성한 내용이 저장되지 않습니다.")==true)
+$(document).ready(function() {
+	$("#cancleBtn").click(function() {
+		if (confirm("글 작성을 취소하시겠습니까? 작성한 내용이 저장되지 않습니다.") == true)
 			history.back();
 	});
 });
 
-
-
 $(function() {
-	var blank=$.cookie("blanked");
- 	var login=$.cookie("logined"); 
- 	var logout=$.cookie("logouted"); 
-		$("#loginBlankDiv1").html(blank);
-		$("#loginBlankDiv2").html(blank);
- 		$("#nicknameDiv").html(login);
- 		$("#logoutDiv").html(logout);
+	var blank = $.cookie("blanked");
+	var login = $.cookie("logined");
+	var logout = $.cookie("logouted");
+	$("#loginBlankDiv1").html(blank);
+	$("#loginBlankDiv2").html(blank);
+	$("#nicknameDiv").html(login);
+	$("#logoutDiv").html(logout);
 });
 
-
-$(document).ready(function(){
-	$("#writeBtn").click(function(){
+$(document).ready(function() {
+	$("#writeBtn").click(function() {
 		user = $("#sessionNickname").val();
-		if(!user){
+		if (!user) {
 			alert("로그인 후 글을 작성하실 수 있습니다.");
-		}else{
-			location.href="/board/write";
-		}	
+		} else {
+			location.href = "/board/write";
+		}
 	});
 });
-	
 
-function DeleteComment(){
-	if(!confirm("댓글을 삭제하시겠습니까?")){
+function DeleteComment() {
+	if (!confirm("댓글을 삭제하시겠습니까?")) {
 		return false;
 	}
-	
 }
 
-function DeletePost(){
-	if(!confirm("게시물을 삭제하시겠습니까?")){
+function DeletePost() {
+	if (!confirm("게시물을 삭제하시겠습니까?")) {
 		return false;
 	}
-	
 }
 
-function writePost(){
-	var content = $("#content").var;
-	var title = $("#title").var;
-	
-	if(typeof title ==="undefined" || title === "" || title === null){
+function writePost() {
+	var content = $("#content").val();
+	var title = $("#title").val();
+	if (typeof title == "undefined" || title == "" || title == null) {
 		alert("제목을 입력해주세요.");
 		return false;
 	}
-	if(typeof content ==="undefined" || content === "" || content === null){
+	else if (typeof content == "undefined" || content == "" || content == null) {
 		alert("내용을 입력해주세요.");
 		return false;
-	}	
+	}
 }
 
 /*function deleteComment(reno, postno){
@@ -71,7 +64,7 @@ function writePost(){
 			alert("댓글이 삭제되었습니다.");
 		}
 	});
-	
-	
+
+
 };
 };*/
