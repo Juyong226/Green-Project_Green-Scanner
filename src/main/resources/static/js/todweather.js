1c21a5348cbbcc3b7bb5ff1a483c8cf9a0ee2908
jw <fers3107@naver.com>
2021-08-28 02:30:04 +0900

    [홍지원] 오늘날씨 date 추가

--- src/main/resources/static/js/todweather.js
+++ src/main/resources/static/js/todweather.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
-
+	function getFormatDate(date){
+		var year = date.getFullYear();
+		var month = (1 + date.getMonth());
+		month = month >= 10 ? month : '0' + month;
+		var day = date.getDate();
+		day = day >= 10 ? day : '0' + day;
+		return year + '-'+ month +'-'+ day  ;
+	}
+	
+	//console.log(tdate);
 	$.ajax({
 		url:'https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=bd4e8036ff9ebd78f21ba7b838d9934e&units=metric',
 		dataType:'json',
 		type:'GET',
 		success:function(data){
-			var $Icon = data.weather[0].icon;
+			//var $Icon = data.weather[0].icon;
 			var $Temp_min = (data.main.temp_min)+'˚';//최저기온
 			var $Temp_max = (data.main.temp_max)+'˚';//최고기온
 			var $city = data.name;
-			
+			var $tdate = getFormatDate(new Date());
 			$('.City').append($city)
 			//$('.CurrIcon').append("http://openweathermap.org/img/wn/"+ $Icon +"@2x.png");
 			$('.CurrTemp').prepend("최저기온 "+$Temp_min);
 			$('.CurrTemph').prepend("최고기온 "+$Temp_max);
+			$('.CurrDate').prepend($tdate);
 			
 		}
 	})
