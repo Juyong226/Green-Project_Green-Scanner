255fd9ab95f0fba5fe1253205946f83ea884902c
Juyong226 <juyong226@naver.com>
2021-11-16 11:34:16 +0900

    피드 이미지 업로드 날짜/시간 log 기록

--- src/main/java/com/garb/gbcollector/util/FileUtils.java
+++ src/main/java/com/garb/gbcollector/util/FileUtils.java
@@ -15,6 +15,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.garb.gbcollector.web.vo.RequestInforVO;
 import com.garb.gbcollector.web.vo.UploadImageVO;
 
 @Component
@@ -23,6 +24,7 @@ public class FileUtils {
 	private final String toDay = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 	private final String feedImgUploadPath = Paths.get("C:", "GreenScanner", "upload", "challenge", toDay).toString();
 	private final long maxImageSize = 5 * 1024 * 1024;
+	private Log log = new Log();
 	/*
 	 * 서버에 생성(저장)할 파일의 이름으로 쓸 랜덤 문자열 반환
 	 * @return 랜덤 문자열
@@ -59,7 +61,7 @@ public class FileUtils {
 	 * @param feedNo: 피드 번호
 	 * @return 업로드 파일 목록
 	 * */
-	public List<UploadImageVO> uploadFeedImages(MultipartFile[] images, Integer feedNo) throws UploadFileException {
+	public List<UploadImageVO> uploadFeedImages(MultipartFile[] images, Integer feedNo, RequestInforVO infor) throws UploadFileException {
 		/* 업로드 파일 정보를 담을 비어있는 리스트 */
 		List<UploadImageVO> uploadList = new ArrayList<>();
 		
@@ -78,8 +80,8 @@ public class FileUtils {
 						/* 서버에 저장할 파일명 만들기 (랜덤 문자열 + 원본 확장자) */
 						final String saveName = getRandomString() + "." + FilenameUtils.getExtension(image.getOriginalFilename());
 						/* 업로드 경로에 saveName으로 파일 생성*/
-						System.out.println("피드 이미지 업로드 날짜: " + toDay);
-						System.out.println("피드 이미지 업로드 시간: " + LocalDateTime.now());
+						log.TraceLog(infor, "피드 이미지 업로드 날짜: " + toDay);
+						log.TraceLog(infor, "피드 이미지 업로드 시간: " + LocalDateTime.now());
 						File target = new File(feedImgUploadPath, saveName);
 						image.transferTo(target);
 						
